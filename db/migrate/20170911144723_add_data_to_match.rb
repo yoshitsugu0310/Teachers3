bd55a10d26a517e472ef793ba8e19dee3ff14485
yuya <yuyatennish@gmail.com>
2017-09-23 10:37:37 +0900

    "USING request_time1::timestamp without time zone"に変更

--- db/migrate/20170911144723_add_data_to_match.rb
+++ db/migrate/20170911144723_add_data_to_match.rb
@@ -1,8 +1,8 @@
 class AddDataToMatch < ActiveRecord::Migration[5.1]
   def change
-     change_column :matches, :request_time1, :datetime
-     change_column :matches, :request_time2, :datetime
-     change_column :matches, :request_time3, :datetime
-     change_column :matches, :appoint_time,  :datetime
+     change_column :matches, :request_time1, "USING request_time1::timestamp without time zone"
+     change_column :matches, :request_time2, "USING request_time2::timestamp without time zone"
+     change_column :matches, :request_time3, "USING request_time3::timestamp without time zone"
+     change_column :matches, :appoint_time,  "USING appoint_time::timestamp without time zone"
   end
 end
