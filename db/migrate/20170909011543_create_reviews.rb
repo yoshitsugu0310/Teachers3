bfc5ea41ba4fe5ca04dd12fcf8c590347c849238
yuya <yuyatennish@gmail.com>
2017-09-23 10:13:39 +0900

    add_indexを削除

--- db/migrate/20170909011543_create_reviews.rb
+++ db/migrate/20170909011543_create_reviews.rb
@@ -6,7 +6,6 @@ class CreateReviews < ActiveRecord::Migration[5.1]
       t.references :user, foreign_key: true
 
       t.timestamps
-    end
-    add_index :reviews, [:user_id, :created_at]
+    
   end
 end
