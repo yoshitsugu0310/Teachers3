52574ce98a554d6b2ce60e8a8d30ec3b5c3e3d65
yuya <yuyatennish@gmail.com>
2017-09-23 14:18:55 +0900

    statusで処理わけを正確に

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
 
   def home
 
-    if logged_in? && current_user.student == nil
+    if logged_in? && current_user.status == "student" && current_user.student == nil
       flash[:info] = 'まずマイページを作りましょう'
       redirect_to new_student_path
 
@@ -10,7 +10,7 @@ class StaticPagesController < ApplicationController
       @feed_items = Teacher.feed(current_user)
     end
 
-    if logged_in? && current_user.teacher == nil
+    if logged_in? && current_user.status == "teacher" && current_user.teacher == nil
       flash[:info] = 'まずマイページを作りましょう'
       redirect_to new_teacher_path
 
