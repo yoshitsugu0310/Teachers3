9e1ee9683970b5aebb8f4402ea9852501fdcad10
yuya <yuyatennish@gmail.com>
2017-09-23 10:32:30 +0900

    datetimeに

--- db/migrate/20170911144723_add_data_to_match.rb
+++ db/migrate/20170911144723_add_data_to_match.rb
@@ -1,8 +1,8 @@
 class AddDataToMatch < ActiveRecord::Migration[5.1]
   def change
-     change_column :matches, :request_time1, 'datetime USING CAST(request_time1 AS datetime)'
-     change_column :matches, :request_time2, 'datetime USING CAST(request_time2 AS datetime)'
-     change_column :matches, :request_time3, 'datetime USING CAST(request_time3 AS datetime)'
-     change_column :matches, :appoint_time, 'datetime USING CAST(appoint_time AS datetime)'
+     change_column :matches, :request_time1, :datetime
+     change_column :matches, :request_time2, :datetime
+     change_column :matches, :request_time3, :datetime
+     change_column :matches, :appoint_time,  :datetime
   end
 end
