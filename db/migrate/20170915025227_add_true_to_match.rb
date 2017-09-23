d02f8670187e2d419fd68884619eb9b89f6d0961
yuya <yuyatennish@gmail.com>
2017-09-23 11:16:01 +0900

    'boolean USING CAST(request AS boolean)'へ

--- db/migrate/20170915025227_add_true_to_match.rb
+++ db/migrate/20170915025227_add_true_to_match.rb
@@ -1,7 +1,7 @@
 class AddTrueToMatch < ActiveRecord::Migration[5.1]
   def change
-    change_column :matches, :request, :boolean
-    change_column :matches, :adoption, :boolean
+    change_column :matches, :request, 'boolean USING CAST(request AS boolean)'
+    change_column :matches, :adoption, 'boolean USING CAST(adoption AS boolean)'
 
   end
 end
