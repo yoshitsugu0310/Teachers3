76607dd46548cb330b1fd1ad12c2840e16d9353c
yuya <yuyatennish@gmail.com>
2017-09-23 11:31:16 +0900

    表示を正確に

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,21 +2,16 @@ class StaticPagesController < ApplicationController
 
   def home
 
-    if logged_in? && (current_user.student == nil || current_user.teacher == nil)
-      if current_user.status == "student"
-        flash[:info] = 'まずマイページを作りましょう'
-        redirect_to new_student_path
-      elsif current_user.status == "teacher"
-        flash[:info] = 'まずマイページを作りましょう'
-        redirect_to new_teacher_path
-      else
+    if logged_in? && current_user.student == nil
+      flash[:info] = 'まずマイページを作りましょう'
+      redirect_to new_student_path
 
-      end
-
-    elsif logged_in? && current_user.status == "student"
+    elsif  logged_in? && current_user.status == "student"
       @feed_items = Teacher.feed(current_user)
 
-
+    elsif logged_in? && current_user.teacher == nil
+      flash[:info] = 'まずマイページを作りましょう'
+      redirect_to new_teacher_path
 
     elsif logged_in? && current_user.status == "teacher"
       @feed_items = Student.feed(current_user)
