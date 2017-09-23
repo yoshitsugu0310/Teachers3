eaabc78259e7b0c680918e46f63363b63667e4ba
yuya <yuyatennish@gmail.com>
2017-09-23 14:16:19 +0900

    if文の作り方

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,12 +8,13 @@ class StaticPagesController < ApplicationController
 
     elsif  logged_in? && current_user.status == "student" && Teacher.any?
       @feed_items = Teacher.feed(current_user)
+    end
 
-    elsif logged_in? && current_user.teacher == nil
+    if logged_in? && current_user.teacher == nil
       flash[:info] = 'まずマイページを作りましょう'
       redirect_to new_teacher_path
 
-    elsif logged_in? && current_user.status == "teacher"
+    elsif logged_in? && current_user.status == "teacher" && Student.any?
       @feed_items = Student.feed(current_user)
 
     end
